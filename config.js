2e70dfe0ad1f555ba34ab280e9e36d0e8b06666d
skyhost546-prog <skyhost546@gmail.com>
2026-06-11 16:01:35 -0300

    BUTTERFLY MD

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PREFIX: "*",
+    AUTO_READ_STATUS: true,
+    AUTO_REACT: false,
+    AUTO_TYPING: false,
+    REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://amalxerv3_db_user:<db_password>@cluster0.h7ueptd.mongodb.net/?appName=Cluster0"
+};
